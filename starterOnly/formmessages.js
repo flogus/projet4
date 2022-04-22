c63754f8c5e63c2c330944551456a8eb44470097
flogus <fg@motionery.com>
2022-04-22 17:29:32 +0200

    feat: ajout des messages dans fichier spécifique

---
+++ starterOnly/formmessages.js
@@ -0,0 +1,7 @@
+const messages = [
+  "Veuillez entrer 2 caractères ou plus pour le champ du nom.",
+  "Vous devez vérifier que vous acceptez les termes et conditions.",
+  "Vous devez choisir une option.",
+  "Vous devez entrer votre date de naissance.",
+  "Merci ! Votre réservation a été reçue.",
+];
